5fbf5260b777b954bc460e45a25a17bfcc80531e
mepps32 <mepps32@gmail.com>
2018-06-15 16:22:42 -0700

    remove unnecessary console logs

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -4,29 +4,26 @@ const Music = ({
   data: {
     allMarkdownRemark: { edges },
   },
-}) => {
-  console.log('data', edges);
-  return (
-    <div id="music" className="h-screen">
-      {edges.map(({ node: { frontmatter } }) => (
-        <a
-          key={frontmatter.title}
-          href={frontmatter.link}
-          className="text-center text-white block py-8 page-padding-top px-8"
-        >
-          {frontmatter.title !== 'Tidal' ? (
-            <i
-              className={`fa fa-3x fa-${frontmatter.title.toLowerCase()}`}
-              aria-hidden="true"
-            />
-          ) : (
-            <div className="text-3xl font-bold ">{frontmatter.title}</div>
-          )}
-        </a>
-      ))}
-    </div>
-  );
-};
+}) => (
+  <div id="music" className="h-screen">
+    {edges.map(({ node: { frontmatter } }) => (
+      <a
+        key={frontmatter.title}
+        href={frontmatter.link}
+        className="text-center text-white block py-8 page-padding-top px-8"
+      >
+        {frontmatter.title !== 'Tidal' ? (
+          <i
+            className={`fa fa-3x fa-${frontmatter.title.toLowerCase()}`}
+            aria-hidden="true"
+          />
+        ) : (
+          <div className="text-3xl font-bold ">{frontmatter.title}</div>
+        )}
+      </a>
+    ))}
+  </div>
+);
 
 export default Music;
 
