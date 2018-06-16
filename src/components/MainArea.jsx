c447950b541afcdef408bf0ace5e5e2ebd5d46c9
Ammon <skatea09@aol.com>
2018-06-16 02:06:07 -0700

    more refractoring

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import Header from './Header';
+import Header from "./Header";
 import BackgroundMedia from "./BackgroundMedia";
-import Pages from './Pages';
+import Pages from "./Pages";
 
-class MainArea extends React.Component {
-  state = { page: "" };
-
-  render() {
-    const { siteTitle, sections, children, url } = this.props;
-    return (
-      <div className="h-screen flex relative bg-black-true">
-        <Header siteTitle={siteTitle} sections={sections} />
-        <BackgroundMedia page={url} />
-        <div className="w-full h-full flex absolute text-black overflow-scroll scrolling-touch">
-          <Pages children={children} page={url} />
-        </div>
-      </div>
-    );
-  }
-}
+const MainArea = ({ siteTitle, sections, children, url }) => (
+  <div className="h-screen flex relative bg-black-true">
+    <Header siteTitle={siteTitle} sections={sections} />
+    <BackgroundMedia page={url} />
+    <div className="w-full h-full flex absolute text-black overflow-scroll scrolling-touch">
+      <Pages children={children} page={url} />
+    </div>
+  </div>
+);
 
 export default MainArea;
