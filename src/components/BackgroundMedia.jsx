667dd4a9499462e985943c1f582fe7c1ea11c70a
Ammon <skatea09@aol.com>
2018-06-16 02:41:30 -0700

    fixes background image

--- src/components/BackgroundMedia.jsx
+++ src/components/BackgroundMedia.jsx
@@ -6,10 +6,10 @@ const BackgroundMedia = ({ page }) => {
     <Motion style={{ opacity: spring(page === "" ? 1 : 0.2) }}>
       {({ opacity }) => (
         <img
-          className="cover min-w-full min-h-full fixed"
+          className="cover min-w-full min-h-full fixed transform-xy-center"
           style={{
             filter: "sepia(100%) brightness(90%) grayscale(60%)",
-            opacity: opacity
+            opacity: opacity,
           }}
           src="https://scontent-lax3-2.cdninstagram.com/vp/32225afe5338117b4aad2c8101440afc/5BA727F2/t51.2885-15/e35/26871991_210236116205238_8712684632765628416_n.jpg"
         />
