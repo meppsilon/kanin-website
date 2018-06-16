ed6a7c4d8f379c6b88874adf8c0f666d37ae843c
Ammon <skatea09@aol.com>
2018-06-16 02:36:58 -0700

    more cleaning

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,34 +1,16 @@
 import React from "react";
-import { Motion, spring } from "react-motion";
 
-const pageState = ["empty", "occupied", "changing"];
-
-class Pages extends React.Component {
-  state = { page: pageState[0] };
-
-  render() {
-    const { page, children } = this.props;
-    return (
-      <Motion
-        style={{
-          margin: spring(page === "empty" ? 0 : page === "changing" ? 40 : 20)
-        }}
-      >
-        {({ margin }) => (
-          <div className="w-full overflow-auto flex justify-center">
-            <div
-              className=""
-              style={{
-                marginTop: `${90}px`
-              }}
-            >
-              {children()}
-            </div>
-          </div>
-        )}
-      </Motion>
-    );
-  }
-}
+const Pages = ({ children }) => (
+  <div className="w-full overflow-auto flex justify-center">
+    <div
+      className=""
+      style={{
+        marginTop: `${90}px`
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+);
 
 export default Pages;
