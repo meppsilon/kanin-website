7679f57aea52ceef231a2cbd1ff9b228f18d70ef
Ammon <skatea09@aol.com>
2018-06-16 02:04:48 -0700

    refractors code

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,7 +1,6 @@
 import React from "react";
+import Header from './Header';
 import BackgroundMedia from "./BackgroundMedia";
-import SocialMedia from "./SocialMedia";
-import Nav from "./Nav";
 import Pages from './Pages';
 
 class MainArea extends React.Component {
@@ -11,18 +10,7 @@ class MainArea extends React.Component {
     const { siteTitle, sections, children, url } = this.props;
     return (
       <div className="h-screen flex relative bg-black-true">
-        <header>
-          <div className="absolute w-full mx-auto text-center pt-6 font-extrabold text-4xl text-white fixed z-10">Kanin</div>
-          <div className="absolute flex transform-y-center fixed z-10" style={{ marginTop: '-20px' }}>
-            <SocialMedia />
-          </div>
-          <nav className="fixed self-center z-10 transform-y-center" style={{ right: '36px' }}>
-            <Nav
-              siteTitle={siteTitle}
-              sections={sections}
-            />
-          </nav>
-        </header>
+        <Header siteTitle={siteTitle} sections={sections} />
         <BackgroundMedia page={url} />
         <div className="w-full h-full flex absolute text-black overflow-scroll scrolling-touch">
           <Pages children={children} page={url} />
