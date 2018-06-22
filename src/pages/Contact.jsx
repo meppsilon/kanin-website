274f9ea3474e3cb957789844126c930132b3ebba
Ammon <skatea09@aol.com>
2018-06-22 12:24:06 -0700

    adds unstyled contact info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import tidalLogo from "../assets/tidal_logo.png";
+import contact from "../content/contact";
 
-const Contact = () => (
-  <div id="contact">
-    <div className="h-full w-full">Yo Yo Yo</div>
-  </div>
-);
+const Contact = ({}) => {
+  const contactCategories = Object.keys(contact);
+  return (
+    <div id="contact" className="h-screen">
+      {contactCategories.map(category => (
+        <div className="text-white">
+          <div>{category}</div>
+          {contact[category].map(contact => (
+            <div>
+              <p>{contact.name}</p>
+              {contact.email && <p>{contact.email}</p>}
+              {contact.phone && <p>{contact.phone}</p>}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Contact;
