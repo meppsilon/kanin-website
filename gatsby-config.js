e4c715da256dd07f677db02227c8d7dcc0c8165c
mepps32 <mepps32@gmail.com>
2018-06-22 16:19:03 -0700

    remove unnecessary plugin in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/assets/favicon.png",
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
