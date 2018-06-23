5b000991fbf7830d49c892779b24b796ea0bdea1
mepps32 <mepps32@gmail.com>
2018-06-22 17:05:31 -0700

    Potentially fix preview bug

--- src/cms/preview-templates/ContactPreviewPage.js
+++ src/cms/preview-templates/ContactPreviewPage.js
@@ -4,7 +4,7 @@ import { ContactPageTemplate } from '../../templates/contact-page'
 
 const ContactPagePreview = ({ entry, widgetFor }) => (
   <ContactPageTemplate
-    categories={entry.getIn(['data', 'categories'])}
+    categories={entry.getIn(['data', 'categories']).toJS()}
   />
 )
 
